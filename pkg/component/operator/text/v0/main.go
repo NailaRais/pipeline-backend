376f07c364b6901dbe4d102eb1959cbd98570464
Naila Rais <157850227+NailaRais@users.noreply.github.com>
2024-11-04 18:58:03 +0530

    Update main.go

--- pkg/component/operator/text/v0/main.go
+++ pkg/component/operator/text/v0/main.go
@@ -1,91 +1,79 @@
-//go:generate compogen readme ./config ./README.mdx
 package text
 
 import (
 	"context"
-	"fmt"
-	"sync"
+	"testing"
 
-	_ "embed"
+	"github.com/frankban/quicktest"
 
 	"github.com/instill-ai/pipeline-backend/pkg/component/base"
+	"github.com/instill-ai/pipeline-backend/pkg/component/internal/mock"
 )
 
-const (
-	taskChunkText string = "TASK_CHUNK_TEXT"
-)
+func TestOperator(t *testing.T) {
+	c := quicktest.New(t)
 
-var (
-	//go:embed config/definition.json
-	definitionJSON []byte
-	//go:embed config/tasks.json
-	tasksJSON []byte
-	once      sync.Once
-	comp      *component
-)
-
-// Operator is the derived operator
-type component struct {
-	base.Component
-}
-
-// Execution is the derived execution
-type execution struct {
-	base.ComponentExecution
-}
-
-// Init initializes the operator
-func Init(bc base.Component) *component {
-	once.Do(func() {
-		comp = &component{Component: bc}
-		err := comp.LoadDefinition(definitionJSON, nil, tasksJSON, nil)
-		if err != nil {
-			panic(err)
-		}
-	})
-	return comp
-}
-
-// CreateExecution initializes a component executor that can be used in a
-// pipeline trigger.
-func (c *component) CreateExecution(x base.ComponentExecution) (base.IExecution, error) {
-	return &execution{ComponentExecution: x}, nil
-}
-
-// Execute executes the derived execution
-func (e *execution) Execute(ctx context.Context, jobs []*base.Job) error {
-
-	for _, job := range jobs {
-		switch e.Task {
-		case taskChunkText:
-			inputStruct := ChunkTextInput{}
-
-			err := job.Input.ReadData(ctx, &inputStruct)
-			if err != nil {
-				job.Error.Error(ctx, err)
-				continue
-			}
-
-			var outputStruct ChunkTextOutput
-			if inputStruct.Strategy.Setting.ChunkMethod == "Markdown" {
-				outputStruct, err = chunkMarkdown(inputStruct)
-			} else {
-				outputStruct, err = chunkText(inputStruct)
-			}
-
-			if err != nil {
-				job.Error.Error(ctx, err)
-				continue
-			}
-			err = job.Output.WriteData(ctx, outputStruct)
-			if err != nil {
-				job.Error.Error(ctx, err)
-				continue
-			}
-		default:
-			job.Error.Error(ctx, fmt.Errorf("not supported task: %s", e.Task))
-			continue
-		}
+	testcases := []struct {
+		name  string
+		task  string
+		input ChunkTextInput
+	}{
+		{
+			name: "chunk texts",
+			task: "TASK_CHUNK_TEXT",
+			input: ChunkTextInput{
+				Text: "Hello world. This is a test.",
+				Strategy: Strategy{
+					Setting: Setting{
+						ChunkMethod: "Token",
+					},
+				},
+			},
+		},
+		{
+			name:  "error case",
+			task:  "FAKE_TASK",
+			input: ChunkTextInput{},
+		},
 	}
-	return nil
+	bc := base.Component{}
+	ctx := context.Background()
+	for i := range testcases {
+		tc := &testcases[i]
+		c.Run(tc.name, func(c *quicktest.C) {
+			component := Init(bc)
+			c.Assert(component, quicktest.IsNotNil)
+
+			execution, err := component.CreateExecution(base.ComponentExecution{
+				Component: component,
+				Task:      tc.task,
+			})
+			c.Assert(err, quicktest.IsNil)
+			c.Assert(execution, quicktest.IsNotNil)
+
+			ir, ow, eh, job := mock.GenerateMockJob(c)
+			ir.ReadDataMock.Set(func(ctx context.Context, v interface{}) error {
+				*v.(*ChunkTextInput) = tc.input
+				return nil
+			})
+			ow.WriteDataMock.Optional().Set(func(ctx context.Context, output interface{}) error {
+				if tc.name == "error case" {
+					c.Assert(output, quicktest.IsNil)
+					return nil
+				}
+				return nil
+			})
+			if tc.name == "error case" {
+				ir.ReadDataMock.Optional()
+			}
+			eh.ErrorMock.Optional().Set(func(ctx context.Context, err error) {
+				if tc.name == "error case" {
+					c.Assert(err, quicktest.ErrorMatches, "not supported task: FAKE_TASK")
+				}
+			})
+			err = execution.Execute(ctx, []*base.Job{job})
+			c.Assert(err, quicktest.IsNil)
+
+		})
+	}
 }
